2935cf04a8afaf2e715b1b0f348785abda488a1b
GAROSDEV <garzonoscar7@gmail.com>
2024-05-09 14:02:51 -0500

    Agregar proyecto editor de codigo JavaScript sencillo

---
+++ 05-EditorCodigo/script.js
@@ -0,0 +1,16 @@
+const codeEditor = document.getElementById("code-editor");
+const outputDiv = document.getElementById("output");
+const runButton = document.getElementById("run-button");
+
+runButton.addEventListener("click", runCode);
+
+function runCode() {
+  const code = codeEditor.value;
+
+  try {
+    const result = eval(code);
+    outputDiv.textContent = result;
+  } catch (error) {
+    outputDiv.textContent = `Error: ${error.message}`;
+  }
+}
